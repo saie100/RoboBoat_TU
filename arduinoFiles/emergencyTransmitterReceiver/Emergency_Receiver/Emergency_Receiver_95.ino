44ecad47cb80aaf244ab1f3d374b1657c2a41e33
GGZhao98 <57693542+GGZhao98@users.noreply.github.com>
2020-02-10 21:33:11 -0500

    Update Emergency_Receiver_95.ino
    Varied from FastReceiverTest

--- arduinoFiles/emergencyTransmitterReceiver/Emergency_Receiver/Emergency_Receiver_95.ino
+++ arduinoFiles/emergencyTransmitterReceiver/Emergency_Receiver/Emergency_Receiver_95.ino
@@ -1,16 +1,8 @@
-// Feather9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Radio Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* for Feather32u4 RFM9x */
-
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 7
@@ -24,147 +16,186 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 // Blinky on receipt
 #define LED 13
 
-// Relay pins
-#define CH1 18  //relay to cutoff circuit
-#define CH2 19  //red LED
-#define CH3 20  //blue LED
-#define CH4 21  //Amber LED
+#define SIZE_radiopacket 30
 
-// ================== Battery Management ================== 
-// Battery voltage measurement pin
-#define VBATPIN A9
+volatile unsigned long missCnt = 0;
 
-// Function for calculating battery percentage
-void measureBattery(float *measured);
+char okMsg[] = "TempleBoatNormal   ";
 
-// flot variable that will store hold the battery voltage;
-float measuredvbat = 0;
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ E-Switch Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define PIN_EMERGENCY_SWITCH 1 /* The pin that the emergency switch is connected to */
+volatile byte EmergencyState_RF = true;
+volatile byte EmergencyState_SW = true;
+volatile byte EmergencyState = true;
 
-int16_t packetnum = 0;
+volatile byte StateChange = true; /* Set to true so the display is updated the first go around*/
 
-void setup() 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Relay Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define PIN_MOTOR_RELAY     18
+#define PIN_RED_LIGHT       19
+#define PIN_BLUE_LIGHT     20
+#define PIN_AMBER_LIGHT      21
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Switch Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define PIN_EMERGENCY_SWITCH 1
+
+// ======================================Battery Monitor Setup=====================================================
+#define VBATPIN              A9
+#define CHRG_V               4.2
+#define EMPT_V               3.0
+
+void setup()
 {
-  Serial.begin(115200);
-  while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  pinMode(PIN_MOTOR_RELAY, OUTPUT);
+  pinMode(PIN_RED_LIGHT, OUTPUT);
+  pinMode(PIN_BLUE_LIGHT, OUTPUT);
+  pinMode(PIN_AMBER_LIGHT, OUTPUT);
 
-  /*Set pinMode and pull up relays' pins(the circuit should be cut off by turning off the relays)*/
-  pinMode(LED, OUTPUT);     
+  pinMode(PIN_EMERGENCY_SWITCH, INPUT);
+  EmergencyState_SW = digitalRead(PIN_EMERGENCY_SWITCH);
+  attachInterrupt(digitalPinToInterrupt(PIN_EMERGENCY_SWITCH), ISR_EmergencyStateChange, CHANGE);
+
+  digitalWrite(PIN_MOTOR_RELAY, HIGH);
+  digitalWrite(PIN_RED_LIGHT, HIGH);
+  digitalWrite(PIN_BLUE_LIGHT, HIGH);
+  digitalWrite(PIN_AMBER_LIGHT, HIGH);
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Serial Monitor Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("Feather LoRa RX Test!");
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Radio Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  pinMode(LED, OUTPUT);
+
   pinMode(RFM95_RST, OUTPUT);
-  pinMode(CH1, OUTPUT);
-  pinMode(CH2, OUTPUT);
-  pinMode(CH3, OUTPUT);
-  pinMode(CH4, OUTPUT);
-  digitalWrite(CH1, HIGH);   //big relay
-  digitalWrite(CH2, HIGH);   //red LED turns off initially
-  digitalWrite(CH3, LOW);  //blue LED (auto mode) turns on
-  digitalWrite(CH4, HIGH);  //amber LED turns off
-  digitalWrite(RFM95_RST, HIGH);
+  /* Manual reset of the radio */
+  digitalWrite(RFM95_RST, HIGH); delay(100);
+  digitalWrite(RFM95_RST, LOW);  delay(10);
+  digitalWrite(RFM95_RST, HIGH); delay(10);
 
-  Serial.println("Feather RFM95 RX Test!");
-  Serial.println();
+  while (!rf95.init()) Serial.println("LoRa radio init failed");
+  Serial.println("LoRa radio init OK!");
+  delay(1000);
 
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-  
-  while (!rf95.init()) {
-    Serial.println("RFM95 radio init failed");
-    while (1);
-  }
-  Serial.println("RFM95 radio init OK!");
-  
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while(1);
-  }
-  Serial.print("Set Freq to: ");  Serial.println(RF95_FREQ);
-  // If you are using a high power rf95 eg RFM95HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf95.setTxPower(23, false);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  if (!rf95.setFrequency(RF95_FREQ)) Serial.println("setFrequency failed");
+  else Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  delay(1000);
+
+  rf95.setTxPower(23, false);
 }
 
-
-void loop() {
-
- /* print battery status*/
- measuredvbat = analogRead(VBATPIN);
- measureBattery(&measuredvbat);
- Serial.print("VBat: " ); Serial.print("%"); Serial.println(measuredvbat*100);
-
- /*Tx and Rx are connected*/
- if (rf95.available()) {
-    // Should be a message for us now   
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    
-    if(measuredvbat<0.1){
-      rf95.send("Receiver Low Battery", sizeof("Receiver Low Battery"));
+// =====================================================================================================
+void loop()
+{
+  if (rf95.waitAvailableTimeout(1000)) {
+    if (rf95.available()) {
+      radioAvailableExecute();
     }
-    // receiving a message from transmitter
-    if (rf95.recv(buf, &len)) {
-      if (!len) return;
-      buf[len] = 0;
-      Serial.print("Received ["); Serial.print(len);  Serial.print("]: ");  Serial.println((char*)buf);
-      Serial.print("RSSI: "); Serial.println(rf95.lastRssi(), DEC);
+    else { /* if radio not available */
+      missCnt++;
+      Serial.println("Radio not available");
+    }
+  }
+  else { /* if radio times-out */
+    missCnt++;
+    Serial.print("Radio timeout");
+  }
+
+  if (missCnt > 2) { /* If three packets are missed in a row, we are now in the emergency state */
+    EmergencyState_RF = true;
+  }
+  
+  EmergencyState = EmergencyState_SW || EmergencyState_RF;
+  stateHandler();
+}
+
+// ====================================================================================================
+void stateHandler() {
+  if (EmergencyState == true) {
       digitalWrite(LED, HIGH);
-      //only sets relays to high if received "OK"
-      if (strstr((char *)buf, "OK")) {
-        digitalWrite(CH1, HIGH);
-        digitalWrite(CH2, HIGH);
-        digitalWrite(CH3, LOW);
-        digitalWrite(CH4, HIGH);
-        // Send a reply! (send back the received words)
-        rf95.send(buf, sizeof(buf));
-        rf95.waitPacketSent();
-        /*rf95.send(measuredvbat, sizeof(float));     //send receiver battery to transmitter
-        rf95.waitPacketSent();*/
-        Serial.println("Sent a reply");
-      }
-      //if don't receive "OK", set relays to low
-      else{
-        digitalWrite(CH1, LOW);
-        digitalWrite(CH2, LOW);
-        digitalWrite(CH3, HIGH);
-        digitalWrite(CH4, HIGH);
-        // Send a reply! (send back the received words)
-        rf95.send(buf, sizeof(buf));
-        rf95.waitPacketSent();
-        Serial.println("Sent a reply");
-      }
-    } 
-    // not receiving a message from transmitter >> set relays to low
-    else {
-      Serial.println("Receive failed >> not receiving message");
-      digitalWrite(CH1, LOW);
-      digitalWrite(CH2, LOW);
-      digitalWrite(CH3, HIGH);
-      digitalWrite(CH4, HIGH);
-    }
- }
- /*Rx lost connection with Tx*/
- else{
-  //lost connecting with transmitter for 5sec >> emergency mode
-  unsigned long startTime = millis();
-  while((millis() - startTime)<3000 );//&& !(rf95.available()));
-  if(!(rf95.available())){
-    Serial.println("Receive failed >> lost connection");
-    digitalWrite(CH1, LOW);
-    digitalWrite(CH2, LOW);
-    digitalWrite(CH3, HIGH);
-    digitalWrite(CH4, HIGH);
+      digitalWrite(PIN_MOTOR_RELAY, HIGH);
+      digitalWrite(PIN_RED_LIGHT, LOW);
   }
- }
- 
+  else {
+      digitalWrite(LED, LOW);
+      digitalWrite(PIN_MOTOR_RELAY, LOW);
+      digitalWrite(PIN_RED_LIGHT, HIGH);
+  }
+
 }
 
-void measureBattery(float *measured){
-  *measured *= 2;    // we divided by 2, so multiply back
-  *measured *= 3.3;  // Multiply by 3.3V, our reference voltage
-  *measured /= 1024; // convert to voltage
-  //Full power: 4.2   Empty power: 3
-  *measured = (*measured - 3)/(4.2 - 3);
+// =====================================================================================================
+void radioAvailableExecute() {
+  // Should be a message for us now
+  uint8_t buf[SIZE_radiopacket];
+  uint8_t len = sizeof(buf);
+
+
+  if (rf95.recv(buf, &len)) {
+    missCnt = 0; /* Reset the miss count if message received */
+    RH_RF95::printBuffer("Received: ", buf, len);
+
+    /* Compare the recieved and the known OK string */
+    int ii;
+    for (ii = 0; (okMsg[ii] == (char)buf[ii] && ((char)buf[ii] != '\0')); ii++);
+    /* The emergency state is set if the received string does not match the OK string */
+    int NormalMessage = (((char)buf[ii] == '\0') && (okMsg[ii] == '\0')); // This is non-zero during normal operation
+ 
+    if (NormalMessage) {
+      EmergencyState_RF = false;
+      blinkLED(); /* Blink LED when not in emergency mode to show connection status */
+    }
+    else{
+      EmergencyState_RF = true;
+    }
+
+    Serial.print("Got: "); Serial.println((char*)buf);
+    Serial.print("RSSI: "); Serial.println(rf95.lastRssi(), DEC);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~Detect Low Battery~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    float batPercent = (measureBattery()-EMPT_V) / (CHRG_V-EMPT_V);
+    if (batPercent > 1) batPercent = 1;
+    if (batPercent < 0.1){
+      rf95.send("Rx<10%", sizeof("Rx<10%"));
+      rf95.waitPacketSent();
+    }
+    else{
+      rf95.send(buf, sizeof(buf));
+      rf95.waitPacketSent();
+    }
+    //rf95.send(buf, sizeof(buf));
+    //rf95.waitPacketSent();
+    Serial.println("Sent a reply");
+  }
+  else { /* if receive fails */
+    missCnt++;
+    Serial.println("Receive failed");
+  }
+}
+
+void blinkLED(void) {
+  digitalWrite(LED, LOW);
+  for (int ii = 0; ii < 10; ii++) delayMicroseconds(10000);
+  digitalWrite(LED, HIGH);
+  for (int ii = 0; ii < 10; ii++) delayMicroseconds(10000);
+  digitalWrite(LED, LOW);
+}
+
+// =========================================Baterry Measurement=========================================
+float measureBattery() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  return measuredvbat;
+}
+
+//==================On-Boat Button=============================================
+void ISR_EmergencyStateChange(void){
+  if(EmergencyState_SW != digitalRead(PIN_EMERGENCY_SWITCH)){
+    delayMicroseconds(50000);
+    if(EmergencyState_SW != digitalRead(PIN_EMERGENCY_SWITCH)){
+      EmergencyState_SW = !EmergencyState_SW;
+    }
+  }
 }
