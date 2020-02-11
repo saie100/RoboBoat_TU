08313bed34f57c8da27d385a8cbfeceaebc29073
GGZhao98 <57693542+GGZhao98@users.noreply.github.com>
2020-02-10 21:31:05 -0500

    Update Emergency_Transmitter_95.ino
    Varied from FastTransmitterTest

--- arduinoFiles/emergencyTransmitterReceiver/Emergency_Transmitter/Emergency_Transmitter_95.ino
+++ arduinoFiles/emergencyTransmitterReceiver/Emergency_Transmitter/Emergency_Transmitter_95.ino
@@ -1,187 +1,255 @@
-// Feather9x_TX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (transmitter)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_RX
 
 #include <SPI.h>
-#include <RH_RF95.h>
-
-// ================= OLED ======================
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <RH_RF95.h>
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OLED Display Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1   // Reset pin # (sharing Arduino reset pin as it is -1)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-// =================================================
 
-/* for feather32u4 */
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ E-Switch Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define PIN_EMERGENCY_SWITCH 1 /* The pin that the emergency switch is connected to */
+volatile byte EmergencyState;
+volatile byte StateChange = true; /* Set to true so the display is updated the first go around*/
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Radio Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 7
 
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
+RH_RF95 rf95(RFM95_CS, RFM95_INT); /* Singleton instance of the radio driver */
+#define LED 13 /* Blinky on receipt */
 
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
+#define SIZE_radiopacket 30
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
-int button_state = 1;
-#define E_BUTTON        1   //interrupt pin for E-stop button
+uint8_t buf[SIZE_radiopacket];
+uint8_t len = sizeof(buf);
 
-#define LED             13
-#define VBATPIN         A9  //Battery pin
+int16_t rssi = 0;
 
-#define EMPTY_VOLTAGE   3   //low battery energy
-#define FULL_VOLTAGE    4.2 //full battery energy
+int16_t successMsgs = 0, failMsgs = 0;
+
+volatile unsigned long missCnt = 0; /* Counts the number of conseuctive misses */
+volatile unsigned long discCnt = 0;
+
+int     RxLowBat = 0;
+char    RxLowBat_MSG[] = "Rx<10%";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Battery Monitor Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define VBATPIN A9
+#define CHRG_V 4.2
+#define EMPT_V 3.0
 
 
-// ==================== OLED PRINTING FUNCTIONS =====================
-#define BAT_ROW        30
-#define BAT_COL        0
+// =====================================================================================================
+void setup() {
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Serial Monitor Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Serial.begin(9600);
+  //while (!Serial.available());
+  delay(2000);
+  Serial.println("The serial monitor has been established");
 
-void oled_displayBattery(void){
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setCursor(BAT_COL, BAT_ROW);
-  display.print("Battery: ");
-  //The voltage of the battery
-  double batteryVoltage = ((double)analogRead(VBATPIN))*2*3.3/1024;
-  //The % charge of the battery
-  int charge = ( (batteryVoltage - EMPTY_VOLTAGE) / (FULL_VOLTAGE - EMPTY_VOLTAGE) ) * 100;
-  display.print(charge);
-  display.print("%");
-}
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Display Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /* Print the success or failure of the serial monitor initialzition in the serial monitor */
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) Serial.println(F("SSD1306 allocation failed"));
+  else Serial.println(F("SSD1306 allocation succeeded"));
+  display.display(); /* Update the output on the display */
+  delay(2000); /* Wait for 2 seconds to ensure that the display has been initialized */
 
-void setup() 
-{
-  Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-  display.display();
-  delay(2000); // Pause for 2 seconds
-  // Clear the buffer
-  display.clearDisplay();
-
-  pinMode(E_BUTTON,INPUT_PULLUP); //setup e_button interrupt pin
-  pinMode(VBATPIN,INPUT);
-  pinMode(LED, OUTPUT);     
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Radio Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(E_BUTTON), e_stop, CHANGE);
-  digitalWrite(RFM95_RST, HIGH);
+  /* Manual reset of the radio */
+  digitalWrite(RFM95_RST, HIGH); delay(100);
+  digitalWrite(RFM95_RST, LOW);  delay(10);
+  digitalWrite(RFM95_RST, HIGH); delay(10);
 
-  Serial.println("Feather RFM95 TX Test!");
-  Serial.println();
-
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-  
-  if (!rf95.init()) {
-    Serial.println("RFM95 radio init failed");
-    display.clearDisplay();
-  
-    display.setTextSize(1);      // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.setCursor(0, 30);     // Start at top-left corner
-    display.print("RFM95 radio init failed");
+  display.clearDisplay(); /* Clear the display */
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    display.println("LoRa init failed");
     display.display();
-    while (1);
   }
-  Serial.println("RFM95 radio init OK!");
-  
-  display.clearDisplay();
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 30);     // Start at top-left corner
-  display.print("RFM95 radio init OK!");
+  Serial.println("LoRa radio init OK!");
+  display.println("LoRa init OK!");
   display.display();
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
+  delay(1000);
+
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
-    while (1);
+    display.println("setFrequency failed");
+    display.display();
+  } else {
+    Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+    display.print("Set Freq to: "); display.println(RF95_FREQ);
+    display.display();
+    delay(1000);
   }
-  // If you are using a high power rf95 eg RFM95HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf95.setTxPower(23, false);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+
+  rf95.setTxPower(23, false);
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ E-Switch Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /* Configure the pin that the switch is connected to to input - hardware pullup installed on board*/
+  pinMode(PIN_EMERGENCY_SWITCH, INPUT);
+  /* Set the emergencyState variable to the state of the emergency switch;s input pin */
+  EmergencyState = digitalRead(PIN_EMERGENCY_SWITCH);
+  /* Enable the ISR for the emergency stop button switch */
+  attachInterrupt(digitalPinToInterrupt(PIN_EMERGENCY_SWITCH), ISR_EmergencyStateChange, CHANGE);
 }
 
+// =====================================================================================================
 void loop() {
-  delay(10);  // Wait 1 second between transmits, could also 'sleep' here!
-  
-  display.clearDisplay();
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  
-  oled_displayBattery();
 
-  if(button_state == 0){ //Emergency functionality
-    char radiopacket[] = "STOP";
-    Serial.print("Sending "); Serial.println(radiopacket);
-    // Send a message!
-    rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
-    rf95.waitPacketSent();
-    display.setCursor(0, 40); 
-    display.print("EMERGENCY!!!");
-    display.setCursor(0, 50);
-    display.print("RSSI: ");
-    display.print(rf95.lastRssi());
-  }
-  else{
-    char radiopacket[] = "OK";  //Everything functioning OKAY
-    Serial.print("Sending "); Serial.println(radiopacket);
-    // Send a message!
-    rf95.send((uint8_t *)radiopacket, strlen(radiopacket));
-    rf95.waitPacketSent(); 
-  
-    display.setCursor(0, 40);     
-    display.print("ALL CLEAR!!!");
-    display.setCursor(0, 50);
-    display.print("RSSI: ");
-    display.print(rf95.lastRssi()); 
+  radio_Update();
+
+  /* Disconnect event occurs after three missed transmissions */
+  if(missCnt == 3){ 
+    discCnt++;
   }
   
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  buf[0] = 0;
-  uint8_t len = RH_RF95_MAX_MESSAGE_LEN;
-  unsigned long listenTime = millis();
-  while((millis() - listenTime) < 2000 && !(rf95.recv(buf, &len)));
-  /*received reply from receiver*/
-  if (buf[0] != 0) {
-    Serial.print("Got a reply: ");
-    Serial.println((char*)buf);
-    display.setCursor(0, 0);     // Start at top-left corner
-    display.print("Connected");
-    display.setCursor(0, 10);
-    display.print((char*)buf);
-  }else{  /*not receiving reply*/
-    listenTime = millis();
-    while((millis() - listenTime) < 2000);
-    if(!(rf95.recv(buf, &len))){
-      Serial.println("Receive failed");
-      display.setCursor(0, 0);     // Start at top-left corner
-      display.print("Disconnected");
-    }else{
-      display.setCursor(0, 0);
-      display.print("Connected");
+  updateDisplay(); /* Update the output on the display */
+  for (int ii = 0; ii < 5; ii++) {
+    delayMicroseconds(10000);
+  }
+}
+
+// =====================================================================================================
+void ISR_EmergencyStateChange() {
+  // Debouncing:
+  /* If the new value does not match the old value, there might have been a state change */
+  if (EmergencyState != digitalRead(PIN_EMERGENCY_SWITCH)) {
+    /* delayMicroseconds() Does not use an ISR timer so it works inside of another ISR */
+    delayMicroseconds(50000);
+    /* If the new value still does not match the old value, there was a state change */
+    if (EmergencyState != digitalRead(PIN_EMERGENCY_SWITCH)) {
+      //radio_Update();
+      EmergencyState = !EmergencyState; /* Update the EmergencyState variable */
+      StateChange = true; /* Set the emergency state to be updated in the loop */
     }
   }
-  display.display();
 }
 
-void e_stop(void){
-  button_state = !button_state;
+// =====================================================================================================
+void radio_Update() {
+  if (digitalRead(PIN_EMERGENCY_SWITCH) == HIGH) radio_TransmitPacket("TempleBoatEmergency");
+  else radio_TransmitPacket("TempleBoatNormal   ");
+
+  if (rf95.waitAvailableTimeout(2000)) {
+    if (rf95.recv(buf, &len)) { /* Should be a reply message for us now */
+      rssi = rf95.lastRssi();
+      successMsgs++;
+      missCnt = 0;
+      /*Check if Receiver is low power (<10%)*/
+      int ii;
+      for (ii = 0; (RxLowBat_MSG[ii] == (char)buf[ii] && ((char)buf[ii] != '%')); ii++);
+      RxLowBat = (((char)buf[ii] == '%') && (RxLowBat_MSG[ii] == '%'));
+      
+      Serial.print("Got reply: "); Serial.println((char*)buf);
+      Serial.print("RSSI: "); Serial.println(rssi, DEC);
+    }
+    else {
+      rssi = 1; /* Invalid RSSI value to indicate no connection */
+      failMsgs++;
+      missCnt++; /* Counts the number of conseuctive misses */
+      Serial.println("Receive failed");
+    }
+  }
+  else {
+    rssi = 2; /* Invalid RSSI value to indicate no connection */
+    failMsgs++;
+    missCnt++; /* Counts the number of conseuctive misses */
+    Serial.println("No reply, is there a listener around?");
+  }
+}
+
+// =====================================================================================================
+void radio_TransmitPacket(char radiopacket[]) {
+
+  //itoa(packetnum++, radiopacket + 13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  //radiopacket[SIZE_radiopacket - 1] = 0;
+
+  Serial.println("Sending...");
+  rf95.send((uint8_t *)radiopacket, SIZE_radiopacket); /* Sending... */
+
+  Serial.println("Waiting for packet to complete...");
+  rf95.waitPacketSent(); /* Waiting for packet to complete... */
+}
+
+// =====================================================================================================
+void updateDisplay() {
+  display.clearDisplay(); /* Clear the display */
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Update the Battery Information on the Display ~~~~~~~~~~~~~~~~~~~~~~~~~
+  float batPercent = (measureBattery()-EMPT_V) / (CHRG_V-EMPT_V);
+  if (batPercent > 1) batPercent = 1;
+  display.setCursor(100, 0); display.print((int)(batPercent * 100)); display.println("%");
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Update the Radio information on the Display ~~~~~~~~~~~~~~~~~~~~~~~~
+  if (rssi < 1) {
+    display.setCursor(0, 0);
+    display.print("RSSI: "); display.println(rssi);
+  }
+  if(missCnt <= 2){
+    display.setCursor(0, 8);
+    display.setTextSize(2);
+    if(RxLowBat == 1){
+      display.print(RxLowBat_MSG);
+    }
+    else  display.print("Connected");
+  }
+  else if (rssi == 1 && missCnt > 2){ /* Ensure that we've missed at least three packets before displaying */
+    display.setCursor(0, 8);
+    display.setTextSize(2);
+    display.print("TIMEOUT");
+  }
+    else if (rssi == 2 && missCnt > 2){ /* Ensure that we've missed at least three packets before displaying */
+    display.setCursor(0, 10);
+    display.setTextSize(2);
+    display.print("NO ACK");
+  }
+  display.setCursor(0, 25);
+  display.setTextSize(1);
+  display.print("Disconnects: "); display.println(discCnt);
+  display.print("Miss/Tot: "); 
+  display.print(failMsgs); display.print("/"); 
+  display.print(failMsgs+successMsgs);
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Update the Emergency Information on the Display ~~~~~~~~~~~~~~~~~~~~~~~~~
+  display.setCursor(0, 45);
+  display.setTextSize(2);
+  if (digitalRead(PIN_EMERGENCY_SWITCH) == HIGH) {
+    display.println("Emergency");
+    Serial.println("Emergency Mode Triggered");
+  } 
+  else {
+    display.println("Normal");
+    Serial.println("Normal Operation");
+  }
+  display.display(); /* Update the display */
+}
+
+
+// =====================================================================================================
+float measureBattery() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  return measuredvbat;
 }
