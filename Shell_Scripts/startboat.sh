f563421bd2802ac27286728d9175c3f45b64a1eb
byrongaspard <16bgaspard@gmail.com>
2020-03-31 15:41:06 +0000

    Updated the website to add camera streaming functionality

--- Shell_Scripts/startboat.sh
+++ Shell_Scripts/startboat.sh
@@ -2,4 +2,4 @@
 gnome-terminal  --window --command="bash -c 'roscore; bash'" \
 		--tab --command="bash -c 'sleep 5; roslaunch rosbridge_server rosbridge_websocket.launch; bash'" \
 		--tab --command="bash -c 'sleep 5; rosrun rosserial_arduino serial_node.py _port:=/dev/ttyACM0; bash'" \
-		--tab --command="bash -c 'cd /home/ubuntu/RoboBoat_TU/boatControlWebsite; python -m SimpleHTTPServer; bash'" 
+		--tab --command="bash -c 'cd /home/ubuntu/RoboBoat_TU/Boat_Website; python -m SimpleHTTPServer; bash'" 
