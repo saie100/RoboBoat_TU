7ee8babf4135cf3797b2888721c090e94ee4d939
byrongaspard <16bgaspard@gmail.com>
2020-04-01 03:24:46 +0000

    Updated GUI, Updated launch shell scripts, modified serial node to allow multiple concurrently

--- Shell_Scripts/killboat.sh
+++ Shell_Scripts/killboat.sh
@@ -4,3 +4,6 @@ pkill -f SimpleHTTPServer
 pkill -f rosbridge
 pkill -f rosserial
 pkill -f roscore
+ssh -t ubuntu@10.19.122.102 "pkill -f zed"
+ssh -t ubuntu@10.19.122.102 "pkill -f web_video_server"
+ssh -t ubuntu@10.19.122.102 "pkill -f rosserial"
--- Shell_Scripts/startboat.sh
+++ Shell_Scripts/startboat.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 gnome-terminal  --window --command="bash -c 'roscore; bash'" \
 		--tab --command="bash -c 'sleep 5; roslaunch rosbridge_server rosbridge_websocket.launch; bash'" \
-		--tab --command="bash -c 'sleep 5; rosrun rosserial_arduino serial_node.py _port:=/dev/ttyACM0; bash'" \
-		--tab --command="bash -c 'cd /home/ubuntu/RoboBoat_TU/Boat_Website; python -m SimpleHTTPServer; bash'" 
+		--tab --command="bash -c 'sleep 5; source ~/catkin_ws/devel/setup.bash; rosrun roboboat_rosserial teensy_serial_node.py; bash'" \
+		--tab --command="bash -c 'cd /home/ubuntu/RoboBoat_TU/Boat_Website; python -m SimpleHTTPServer; bash'" \
+		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-zed.sh;'" \
+		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-web_video_server.sh;'" \
+		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-rosserial_battery.sh;'"
+
