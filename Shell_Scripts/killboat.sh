9d7338a437015f5f271e656d67b9a1e9464f18b3
byrongaspard <16bgaspard@gmail.com>
2020-04-16 21:20:15 +0000

    updated start and stop shell scripts for rplidar

--- Shell_Scripts/killboat.sh
+++ Shell_Scripts/killboat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+ssh -t ubuntu@10.19.122.102 "bash -ci 'rosservice call /stop_motor'" 
 pkill -f SimpleHTTPServer
 pkill -f rosbridge
 pkill -f rosserial
-pkill -f roscore
 ssh -t ubuntu@10.19.122.102 "pkill -f zed"
 ssh -t ubuntu@10.19.122.102 "pkill -f web_video_server"
 ssh -t ubuntu@10.19.122.102 "pkill -f rosserial"
 ssh -t ubuntu@10.19.122.102 "pkill -f rplidar"
+sleep 5 && pkill -f roscore
--- Shell_Scripts/startboat.sh
+++ Shell_Scripts/startboat.sh
@@ -5,5 +5,6 @@ gnome-terminal  --window --command="bash -c 'roscore; bash'" \
 		--tab --command="bash -c ' ./start-website.sh; bash'" \
 		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-zed.sh;'" \
 		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-web_video_server.sh;'" \
+		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-rplidar.sh;'" \
 		--tab --command="bash -c 'sleep 7; ssh -t ubuntu@10.19.122.102 bash -c ./RoboBoat_TU/Shell_Scripts/start-rosserial_battery.sh;'"
 
