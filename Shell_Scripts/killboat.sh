bd64f4ec3307163e43420a633201ab47ea6b1af1
byrongaspard <16bgaspard@gmail.com>
2020-04-16 19:02:48 +0000

    Added shell script to start RPLIDAR

--- Shell_Scripts/killboat.sh
+++ Shell_Scripts/killboat.sh
@@ -7,3 +7,4 @@ pkill -f roscore
 ssh -t ubuntu@10.19.122.102 "pkill -f zed"
 ssh -t ubuntu@10.19.122.102 "pkill -f web_video_server"
 ssh -t ubuntu@10.19.122.102 "pkill -f rosserial"
+ssh -t ubuntu@10.19.122.102 "pkill -f rplidar"
