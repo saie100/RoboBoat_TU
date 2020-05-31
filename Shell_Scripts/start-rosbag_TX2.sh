690bc02de9d083b912ddd9c713c022780229f219
byrongaspard <56890175+byrongaspard@users.noreply.github.com>
2020-05-30 23:59:34 -0400

    Update start-rosbag_TX2.sh

--- Shell_Scripts/start-rosbag_TX2.sh
+++ Shell_Scripts/start-rosbag_TX2.sh
@@ -6,12 +6,6 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S");
 
 file_name="TU_RoboBoat.$current_time";
 
-echo "";
-echo "";
-echo $file_name;
-echo "";
-echo "";
-
 cd ~/rosbag_files &&
 rosbag record -O $file_name /BatteryVoltage /ControlMode /IMU_absoluteOrientation /IMU_angularVelocity /IMU_eulerOrientation /IMU_linearAcceleration /IMU_magVec /Motor_leftPWM /Motor_rightPWM /gps /zed/zed_node/stereo_raw/image_raw_color /zed/zed_node/depth/depth_registered /scan &&
 sleep 5
