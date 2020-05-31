be1c65c2ba73127d065e7906f1deb2f8dcd74818
temple-roboboat <temple.boat@gmail.com>
2020-05-30 21:40:01 -0400

    modifed rosbag start/stop

--- Shell_Scripts/start-rosbag_TX2.sh
+++ Shell_Scripts/start-rosbag_TX2.sh
@@ -2,4 +2,4 @@
 #export ROS_IP=0.0.0.0;
 export ROS_MASTER_URI=http://10.19.122.101:11311/;
 source ~/catkin_ws/devel/setup.bash;
-cd ~/rosbag_files && touch test.txt;
+cd ~/rosbag_files && rosbag record -O subset /BatteryVoltage /ControlMode /IMU_absoluteOrientation /IMU_angularVelocity /IMU_eulerOrientation /IMU_linearAcceleration /IMU_magVec /Motor_leftPWM /Motor_rightPWM /gps /zed/zed_node/stereo_raw/image_raw_color /zed/zed_node/depth/depth_registered /scan && sleep 5;
