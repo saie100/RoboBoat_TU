bfae43a4bbbca1401a37b31dd4d9ff084d21c118
temple-roboboat <temple.boat@gmail.com>
2020-05-30 22:10:03 -0400

    Added timestamp to rosbag recording

--- Shell_Scripts/start-rosbag_TX2.sh
+++ Shell_Scripts/start-rosbag_TX2.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-#export ROS_IP=0.0.0.0;
 export ROS_MASTER_URI=http://10.19.122.101:11311/;
 source ~/catkin_ws/devel/setup.bash;
-cd ~/rosbag_files && rosbag record -O subset /BatteryVoltage /ControlMode /IMU_absoluteOrientation /IMU_angularVelocity /IMU_eulerOrientation /IMU_linearAcceleration /IMU_magVec /Motor_leftPWM /Motor_rightPWM /gps /zed/zed_node/stereo_raw/image_raw_color /zed/zed_node/depth/depth_registered /scan && sleep 5;
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S");
+
+file_name="TU_RoboBoat.$current_time";
+
+echo "";
+echo "";
+echo $file_name;
+echo "";
+echo "";
+
+cd ~/rosbag_files &&
+rosbag record -O $file_name /BatteryVoltage /ControlMode /IMU_absoluteOrientation /IMU_angularVelocity /IMU_eulerOrientation /IMU_linearAcceleration /IMU_magVec /Motor_leftPWM /Motor_rightPWM /gps /zed/zed_node/stereo_raw/image_raw_color /zed/zed_node/depth/depth_registered /scan &&
+sleep 5
+
